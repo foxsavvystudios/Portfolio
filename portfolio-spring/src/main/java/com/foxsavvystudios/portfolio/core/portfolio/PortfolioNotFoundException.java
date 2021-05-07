7e595357cb93b09791a0c03bc87ac8972da569a2
lsuman83 <sean.j.bordelon@gmail.com>
2021-05-07 01:19:16 -0400

    Create PortfolioNotFoundException

---
+++ portfolio-spring/src/main/java/com/foxsavvystudios/portfolio/core/portfolio/PortfolioNotFoundException.java
@@ -0,0 +1,8 @@
+package com.foxsavvystudios.portfolio.core.portfolio;
+
+public class PortfolioNotFoundException extends Exception {
+
+    private static final String MESSAGE_TEMPLATE = "No Portfolio exists for Id: %d";
+
+    public PortfolioNotFoundException(Long id) { super(String.format(MESSAGE_TEMPLATE, id)) ; }
+}
