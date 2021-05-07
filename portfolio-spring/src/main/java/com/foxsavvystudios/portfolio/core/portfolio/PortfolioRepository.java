3e524eb78b3bd746515aecd7dbda06e1268ec051
lsuman83 <sean.j.bordelon@gmail.com>
2021-05-07 01:18:58 -0400

    Create PortfolioRepository interface

---
+++ portfolio-spring/src/main/java/com/foxsavvystudios/portfolio/core/portfolio/PortfolioRepository.java
@@ -0,0 +1,6 @@
+package com.foxsavvystudios.portfolio.core.portfolio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
+}
