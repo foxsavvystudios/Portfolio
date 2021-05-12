68855ba8742e87b436d74f30361f8861a2ede88b
lsuman83 <sean.j.bordelon@gmail.com>
2021-05-12 12:20:53 -0400

    removing unnecessary files

--- portfolio-spring/src/main/java/com/foxsavvystudios/portfolio/core/portfoliofile/PortfolioFileNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.foxsavvystudios.portfolio.core.portfoliofile;
-
-public class PortfolioFileNotFoundException extends Exception {
-
-    private static final String MESSAGE_TEMPLATE = "No Portfolio File exists for this id: %d";
-
-    public PortfolioFileNotFoundException(Long id) { super(String.format(MESSAGE_TEMPLATE, id)); }
-
-}
